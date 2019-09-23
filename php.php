8f952678fc1742bffedd304ba2c307625fe1817a
dostaurelien <54935526+dostaurelien@users.noreply.github.com>
2019-09-23 11:35:21 +0200

    Create php.php

---
+++ php.php
@@ -0,0 +1,18 @@
+<?php
+
+$film = 'Indiana Jones et la dernière croisade';
+$filmvu= 'True';
+$anneedesortie='1983';
+$notefilm="7.4";
+
+echo $film; 
+echo '<br>';
+echo $filmvu;
+echo '<br>';
+echo $anneedesortie;
+echo '<br>';
+echo $notefilm;
+?>
+
+
+ 
